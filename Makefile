67dc91a8c08a7de7e5f38192b88a49adb80d30b4
nm <nagamocha@gmail.com>
2022-06-03 01:45:25 +0300

    Init

---
+++ Makefile
@@ -0,0 +1,55 @@
+# =============================================================================
+.DEFAULT_GOAL:=help
+.SILENT:
+SHELL:=/usr/bin/bash
+
+
+# =============================================================================
+# 			DEV
+# =============================================================================
+#
+PROJECT_NAME=resume
+VENV_DIR:=.venv
+VENV_BIN:=.venv/bin/
+ACTIVATE:=source .venv/bin/activate &&
+
+.PHONY: help setup venv run lint format test build pre-commit coverage
+
+help:
+	echo -e "\nResume generator. Run make gen to generate resume\n"
+
+venv:
+	test -d $(VENV_DIR) || python3 -m venv $(VENV_DIR)
+
+setup:
+	test -d $(VENV_DIR) || python3 -m venv $(VENV_DIR)
+	poetry install
+	$(VENV_BIN)pre-commit install
+
+build/resume.html: resume/resume.md resume/generate.py resume/base.html resume/style.css
+	echo "here"
+	python resume/generate.py -d build
+
+gen: build/resume.html
+	echo "foo"
+
+lint:
+	flake8 --show-source .
+	bandit -q -r -c "pyproject.toml" .
+
+format:
+	black .
+
+test:
+	pytest
+
+build:
+	poetry build -q
+
+pre-commit:
+	pre-commit run --all-files
+
+clean:
+	rm -rf $(VENV_DIR) build/*
+	find . -type d -name '__pycache__' -exec rm -rf {} +
+# =============================================================================
---
+++ resume/generate.py
@@ -0,0 +1,105 @@
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import argparse
+import logging
+import os
+import pathlib
+import re
+import sys
+
+import markdown
+
+preamble = """\
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>{title}</title>
+<style>
+{css}
+</style>
+</head>
+<body>
+<div id="resume">
+"""
+
+postamble = """\
+</div>
+</body>
+</html>
+"""
+
+
+def title(md: str) -> str:
+    """
+    Return the contents of the first markdown heading in md, which we
+    assume to be the title of the document.
+    """
+    for line in md.splitlines():
+        if re.match("^#[^#]", line):  # starts with exactly one '#'
+            return line.lstrip("#").strip()
+    raise ValueError(
+        "Cannot find any lines that look like markdown h1 headings to use as"
+        " the title"
+    )
+
+
+def generate_html(md: str, css: str) -> str:
+    return "".join(
+        (
+            preamble.format(title=title(md), css=css),
+            markdown.markdown(md, extensions=["smarty"]),
+            postamble,
+        )
+    )
+
+
+if __name__ == "__main__":
+    # parse commandline arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--output-dir", default=os.getcwd(), nargs="?")
+    parser.add_argument("-q", "--quiet", action="store_true")
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+
+    # config logging level
+    if args.quiet:
+        log_level = logging.WARN
+    elif args.debug:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+    logging.basicConfig(level=log_level, format="%(message)s")
+
+    # get output dir for generated html
+    output_dir = args.output_dir
+    if not os.path.isdir(output_dir):
+        sys.stderr.write(f"Invalid path to output dir: {output_dir}\n")
+        sys.exit(1)
+
+    resume_dir = pathlib.Path(__file__).parent.resolve()
+
+    # get resume markdown content
+    resume_markdown_path = os.path.join(resume_dir, "resume.md")
+    with open(resume_markdown_path, encoding="utf-8") as f:
+        resume_markdown = f.read()
+
+    # get css for styling resume
+    css_path = os.path.join(resume_dir, "style.css")
+    with open(css_path, encoding="utf-8") as f:
+        css = f.read()
+
+    generated_html = generate_html(resume_markdown, css)
+
+    # write out generated html
+    output_html_path = os.path.join(output_dir, "resume.html")
+    with open(output_html_path, "w", encoding="utf-8") as f:
+        f.write(generated_html)
+        logging.info(f"Written to: {f.name}")
